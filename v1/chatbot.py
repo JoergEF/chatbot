ae6f8e0b8fe83c30bf388f4cc7a18eab38f71cdf
Jörg Hofmann <joerg@thuenic.de>
2020-12-17 13:00:06 +0100

    v1 fertig

--- v1/chatbot.py
+++ v1/chatbot.py
@@ -5,27 +5,35 @@
 # (c) 2020 by me, Lizenz GPLv3
 
 import random
-# zufallsantworten = ["Oh wirklich...", "Interessant", "Das kann man so sehen.", "Ich verstehe..."]
+
+# zufällige Antworten
+zufallsantworten = ["Oh wirklich...", "Interessant", "Das kann man so sehen.", "Ich verstehe..."]
+
 # Stopwörter
 reaktionen = {"hallo": "aber hallo",
-                "geht": "Was verstehst Du darunter",
-                "schmeckt": "Ich habe keinen Geschmackssinn"
-             }
+              "geht": "Was verstehst Du darunter",
+              "schmeckt": "Ich habe keinen Geschmackssinn"}
 
+# Ausgabe Head..
 print("Willkommen beim ChatBot (v1)")
 print("Worüber wollen Sie sprechen")
 print("Zum Beenden geben Sie bye ein...")
 print("")
 
+# main
 nutzereingabe = ""
 while nutzereingabe != "bye":
     nutzereingabe = ""
-    nutzereingabe = input("Ihre Frage oder Antwort: ")
+    while nutzereingabe == "":
+        nutzereingabe = input("Ihre Frage oder Antwort: ")
     nutzereingabe = nutzereingabe.lower()
     nutzerwoerter = nutzereingabe.split()
+
+    intelligentAntworten = False
     for einzelwoerter in nutzerwoerter:
         if einzelwoerter in reaktionen:
             print(reaktionen[einzelwoerter])
-            
-    # print(random.choice(zufallsantworten))
+            intelligentAntworten = True
+    if intelligentAntworten == False:
+        print(random.choice(zufallsantworten))
 print("einen schönen Tag.")
