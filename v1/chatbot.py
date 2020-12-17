ef11f11564444fd60dbd73d663159c844416d404
Jörg Hofmann <joerg@thuenic.de>
2020-12-17 11:12:08 +0100

    Initialer Commit

---
+++ v1/chatbot.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Ein einfacher ChatBot
+# (c) 2020 by me, Lizenz GPLv3
+
+print("Willkommen beim ChatBot (v1)")
+print("Worüber wollen Sie sprechen")
+print("Zum Beenden geben Sie bye ein...")
+print("")
+
+nutzereingabe = ""
+nutzereingabe = input("Ihre Frage oder Antwort: ")
+print(nutzereingabe)
